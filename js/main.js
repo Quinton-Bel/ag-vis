43a826c3e2c6b311d94745c94cb8682fba774867
Quinton <quinton.bel@hotmail.com>
2020-04-08 22:06:56 -0400

    Co-authored-by: Yudi Tao <yudi.tao@ontariotechu.net>

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var w = innerWidth;
-var h = innerHeight;
-var hr = 5;
+var w = 500;
+var h = 300;
+var hr = 3;
+var selectedMonth = 0
+var selectedYear = "1990"
 function saveTextAsFile(t){
 		var textFileAsBlob = new Blob([t], {type:"application/json"});
 		var downloadLink = document.createElement("a");
@@ -24,36 +26,23 @@ function saveTextAsFile(t){
 
 		downloadLink.click();
 }
+function cordToKey(cord){
+  return cord[0].toString()+" "+cord[1].toString()
+}
+function cordRound(cord){
+  return [Math.round( cord[0] * 100 + Number.EPSILON ) / 100,
+          Math.round( cord[1] * 100 + Number.EPSILON ) / 100]
+}
 window.onload = function () {
   var svg = d3.select("#canvas")
     .attr("height", h)
     .attr("width", w)
-    .attr("viewBox", [0, 0, w, h]);
-
-  var gdata = d3.json("canada.topojson")
-    .then(function (ca) {
-      var feature = topojson.feature(ca, ca.objects.collection)
-      var proj = d3.geoAlbers()
-        .fitSize([w * 0.8, h * 0.8], feature)
-        .translate([w / 2, h])
-      var path = d3.geoPath().projection(proj);
-      var map = svg.append("g")
-      console.log(feature)
-      //      map
-      //        .datum(feature)
-      //        .append("path")
-      //        .attr("class", "provience")
-      //        .attr("fill", "white")
-      //        .attr("stroke", "black")
-      //        .attr("d", path);
-
-    });
+    .attr("viewBox", [0, 0, w, h]); 
 
   var ca_b = d3.json("canadaBorder.geo.json")
-    .then(function (ca) {
+    .then(function (ca){ 
       var proj = d3.geoAlbers()
-        .fitSize([w * 0.8, h * 0.8], ca)
-        .translate([w / 2, h])
+        .fitSize([w*0.95, h*0.90], ca)
       var path = d3.geoPath().projection(proj);
       var hex = d3.hexgrid()
         .extent([w, h])
@@ -62,23 +51,61 @@ window.onload = function () {
         .projection(proj)
         .hexRadius(hr)
       var map = svg.append("g")
-
       var hexmap = hex(ca)
-	
-	var cords = hexmap.grid.layout.map((x) => {return [x.x, x.y]})
-	saveTextAsFile(JSON.stringify({"cords": cords}))
-    console.log(JSON.stringify({"cords": cords}))
+      //Gets the cords for preprocessing
+      // var cords = hexmap.grid.layout.map((x) => {
+      //       var latLon = proj.invert([x.y, x.x])
+      //       })
+      //saveTextAsFile(JSON.stringify({"cords": cords}))
+      //console.log(JSON.stringify({"cords": cords}))
+      var ca_b = d3.json("tout.json")
+      .then(function (weatherData){
+        var slider = d3
+        .sliderHorizontal()
+        .min(0)
+        .max(10)
+        .step(1)
+        .width(300)
+        .displayValue(false)
+        .on('onchange', val => {
+          d3.select('#value').text(val);
+        });
+    
+      d3.select('#slider')
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)')
+        .call(slider);
 
-      svg.selectAll('.hex')
-        .data(hexmap.grid.layout)
-        .enter()
-        .append('path')
-        .attr('class', 'hex')
-        .attr('transform', d => `translate(${d.x} ${d.y})`)
-        .attr('d', hexmap.hexagon())
-        .style('stroke', '#666')
-        .style('fill', '#fff')
-        .style('stroke-width', 1);
-    });
+        var cScale = d3.scaleLinear()
+                        .domain([-5, 30])
+                        .range(['white', 'blue'])
+        var yearlyData = weatherData[selectedYear]
+        svg.selectAll('.hex')
+          .data(hexmap.grid.layout)
+          .enter()
+          .append('path')
+          .attr('class', 'hex')
+          .attr('transform', d => 'translate('+d.x+','+d.y+')')
+          .attr('d', hexmap.hexagon())
+          .style('stroke', '#666')
+          .style('fill', (d)=>{
+            var hexLonLat = cordRound(proj.invert([d.y, d.x]))
+            if (cordToKey(hexLonLat) in yearlyData){
+              var hexData = yearlyData[cordToKey(hexLonLat)][selectedMonth]
+              if (hexData != null){
+                return cScale(hexData)
+              } 
+            }
+            return 'gray'
+          })
+          .style('stroke-width', 1);
+      })
+      
+   
+   
+      }); // end then()
 
 }
